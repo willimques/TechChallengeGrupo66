a22018b1fb5703f8bdf8f4e5863c1e5b8e9acfa1
marcos paulo <marcos_pfc@hotmail.com>
2024-05-22 10:13:36 -0300

    metodo para consulta contatatos por região

--- Infra.Data/Repository/ContatoRepository.cs
+++ Infra.Data/Repository/ContatoRepository.cs
@@ -1,24 +1,26 @@
 ﻿using Domain.Entities;
 using Domain.Entities.Enum;
 using Infra.Data.Interfaces;
-using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Dapper;
 
 namespace Infra.Data.Repository
 {
     public class ContatoRepository : Repository<Contato>, IContatoRepository
     {
+        private readonly IDbConnection _context;
         public ContatoRepository(IDbConnection context) : base(context)
         {            
+            _context = context;
         }
 
         public Task<IEnumerable<Contato>> GetAllByRegionAsync(RegionsType idRegiao)
-        {
-            throw new NotImplementedException();
+        { 
+            var sql = @"SELECT c.*, d.* FROM [dbo].[CONTATOS] c (NOLOCK)
+                            JOIN [dbo].[DDD] d (NOLOCK) ON c.DDD_ID=d.id
+                        WHERE d.regiao = @idRegiao ";
+            
+            return _context.QueryAsync<Contato>(sql, new { idRegiao });
         }
     }
 }
