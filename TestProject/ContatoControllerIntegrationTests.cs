7c714dddaea5b42c532ec36f7ce0d27c9fd6cf47
marcos paulo <marcos_pfc@hotmail.com>
2024-07-24 23:41:22 -0300

    fix

--- TestProject/ContatoControllerIntegrationTests.cs
+++ TestProject/ContatoControllerIntegrationTests.cs
@@ -34,7 +34,8 @@ namespace TestProject
                         services.AddSingleton(_mockContatoService.Object);
                     });
                 });
-            _factory.Server.BaseAddress = new Uri("https://localhost:5000");
+            _factory.Server.BaseAddress = new Uri("http://localhost:5000");
+            _factory.ClientOptions.BaseAddress = new Uri("http://localhost:5000"); 
 
             _client = _factory.CreateClient();
          
